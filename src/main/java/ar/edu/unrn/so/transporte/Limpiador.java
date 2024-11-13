03fb6b27151a74586d017a9c483b2199471a237e
AlvaroIgnacio <>
2024-11-13 16:56:52 -0300

    El limpiador trabaja con puentes de dos manos

--- src/main/java/ar/edu/unrn/so/transporte/Limpiador.java
+++ src/main/java/ar/edu/unrn/so/transporte/Limpiador.java
@@ -1,13 +1,19 @@
 package ar.edu.unrn.so.transporte;
 
+/**
+ * Sistemas Operativos 2024
+ * Sistema de transporte (ejercicio 3.4)
+ * Proceso de limpieza automática de puentes
+ * @author Álvaro Bayón
+ */
 public class Limpiador extends Thread {
 	
     private final Puente puente1;
-    private final Puente puente2;
+    private final PuenteDosManos puente2;
     private boolean limpiarPuente1 = true; 
     private boolean stop = false;
 
-    public Limpiador(Puente puente1, Puente puente2) {
+    public Limpiador(Puente puente1, PuenteDosManos puente2) {
         this.puente1 = puente1;
         this.puente2 = puente2;
     }
@@ -16,7 +22,8 @@ public class Limpiador extends Thread {
     public void run() {
         try {
             while (!stop) {
-            	int tiempoEspera = (int) (Math.random() * 1000) + 500;
+            	//El proceso esta detenido un tiempo aleatorio.
+            	int tiempoEspera = (int) (Math.random() * 500) + 500;
                 Thread.sleep(tiempoEspera);
 
                 // Alternamos entre los puentes para limpiarlos
@@ -27,6 +34,7 @@ public class Limpiador extends Thread {
                     long tiempoLimpieza = (long) (Math.random() * 2000) + 1000;
                     Thread.sleep(tiempoLimpieza);  
                     
+                    // Se habilita el paso nuevamente
                     System.out.println("El limpiador TERMINÓ de limpiar el puente " + puente1.nombre());
                     puente1.terminarLimpieza();
                 } else {
@@ -36,49 +44,20 @@ public class Limpiador extends Thread {
                     long tiempoLimpieza = (long) (Math.random() * 2000) + 1000;
                     Thread.sleep(tiempoLimpieza);
 
+                    // Se habilita el paso nuevamente
                     System.out.println("El limpiador TERMINÓ de limpiar el puente " + puente2.nombre());
                     puente2.terminarLimpieza();
                 }
 
-                // Cambiar el puente a limpiar para el próximo ciclo
+                // Alternamos entre los puentes para limpiarlos
                 limpiarPuente1 = !limpiarPuente1;
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
-	
-	/*
-    private final Puente puente;
-    private boolean stop = false;
-
-    public Limpiador(Puente puente) {
-        this.puente = puente;
-    }
-
-    @Override
-    public void run() {
-        try {
-            while (!stop) {
-            	int tiempoEspera = (int) (Math.random() * 1000) + 500;
-                Thread.sleep(tiempoEspera);
-
-                // Comienza el proceso de limpieza
-            	System.out.println("El limpiador ha comenzado a limpiar el puente "+puente.nombre()+". NO SE PUEDE PASAR!");
-                this.puente.iniciarLimpieza();
-
-                int tiempoLimpieza = (int) (Math.random() * 3000) + 2000;  
-                Thread.sleep(tiempoLimpieza);
-
-                System.out.println("El limpiador TERMINÓ de limpiar el puente " + puente.nombre());
-                this.puente.terminarLimpieza();  
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }*/
-    
-    public void makeStop() {
+	    
+    public void detener() {
     	this.stop = true;
     }
 
